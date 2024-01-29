c23c4a062cc704629f11d23ee6bc2ed4f907d168
volik97 <volik97@inbox.ru>
2024-01-29 12:49:21 +0300

    ll

--- server.js
+++ server.js
@@ -80,9 +80,11 @@ router.post('/message', async (ctx, next) => {
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 7070;
+console.log(app.callback())
 const server = http.createServer(app.callback());
+
 const wsServer = new WS.Server({ server });
-console.log(wsServer)
+
 wsServer.on('connection', (ws, req) => {
   const errCallback = (err) => {
     if(err){
